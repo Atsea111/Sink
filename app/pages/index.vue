104af1ed05b69bd05af59d21d7bf264cd6c54fc9
Atsea111 <127626814+Atsea111@users.noreply.github.com>
2026-08-16 15:03:24 +0800

    Revamp index.vue layout and content

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,14 +1,20 @@
 <template>
-  <div
-    class="
-      flex flex-col justify-center overflow-x-clip bg-background text-foreground
-    "
-  >
-    <HomeHero />
-    <HomeLogos />
-    <HomeFeatures />
-    <HomeStats />
-    <HomeTestimonials />
-    <HomeCta />
-  </div>
+  <main class="min-h-screen flex items-center justify-center">
+    <div class="text-center">
+      <h1 class="text-4xl font-bold">
+        AT
+      </h1>
+
+      <p class="mt-4 text-gray-500">
+        Personal Link Service
+      </p>
+
+      <a
+        href="https://你的另一个网站.com"
+        class="mt-6 inline-block"
+      >
+        进入主页
+      </a>
+    </div>
+  </main>
 </template>
